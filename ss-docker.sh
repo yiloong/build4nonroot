bc8829c2677106ab8fce1fc47d69aea624fce9e8
YiloongGu <jack@kuilo.ltd>
2020-09-10 18:44:13 +0800

    update shadowsocks dependences

--- ss-docker.sh
+++ ss-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Auther: yiloong
-# Date: 2020-02-28
-# Ver: 1.3
+# Date: 2020-09-10
+# Ver: 1.4
 # Build shadowsocks-libev statically.
 # To build the final proj statically, all its depdpkgs needs built statically too.
 
@@ -12,27 +12,27 @@ SS_PATH="${BUILD_PATH}/out/shadowsocks-libev-${SS_VER}"
 cpu=4
 
 wget 172.17.0.1:8000/shadowsocks-libev-${SS_VER}.tar.gz
-wget 172.17.0.1:8000/libev-4.31.tar.gz
-wget 172.17.0.1:8000/mbedtls-2.7.14-apache.tgz
-wget 172.17.0.1:8000/libsodium-1.0.18.tar.gz
+wget 172.17.0.1:8000/libev-4.33.tar.gz
+wget 172.17.0.1:8000/mbedtls-2.7.15-apache.tgz
+wget 172.17.0.1:8000/libsodium-1.0.18-stable.tar.gz
 wget 172.17.0.1:8000/pcre-8.44.tar.gz
-wget 172.17.0.1:8000/c-ares-1.15.0.tar.gz
+wget 172.17.0.1:8000/c-ares-1.16.1.tar.gz
 apt install xmlto asciidoc
 
-tar xf libev-4.31.tar.gz
-cd libev-4.31
+tar xf libev-4.33.tar.gz
+cd libev-4.33
 ./configure --prefix=$DEP_PATH --enable-static --disable-shared
 make -j$cpu
 make install
 cd ..
 
-tar xf mbedtls-2.7.14-apache.tgz
-cd mbedtls-2.7.14
+tar xf mbedtls-2.7.15-apache.tgz
+cd mbedtls-2.7.15
 make -j$cpu DESTDIR=$DEP_PATH LDFLAGS=-static install
 cd ..
 
-tar xf libsodium-1.0.18.tar.gz
-cd libsodium-1.0.18
+tar xf libsodium-1.0.18-stable.tar.gz
+cd libsodium-stable
 ./configure --prefix=$DEP_PATH --enable-static --disable-shared
 make -j$cpu
 make install
@@ -45,8 +45,8 @@ make -j$cpu
 make install
 cd ..
 
-tar xf c-ares-1.15.0.tar.gz
-cd c-ares-1.15.0
+tar xf c-ares-1.16.1.tar.gz
+cd c-ares-1.16.1
 ./configure --prefix=$DEP_PATH --enable-static --disable-shared
 make -j$cpu
 make install
@@ -59,5 +59,5 @@ make -j$cpu
 make install
 cd ..
 cd out/
-tar czf shadowsocks-libev-${SS_VER}-docker.tar.gz shadowsocks-libev-3.3.4
+tar czf shadowsocks-libev-${SS_VER}-docker.tar.gz shadowsocks-libev-${SS_VER}
 echo "SUCCESS"
