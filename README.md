b0bb57fe3095a520c3955a91e81a52f470498cee
YiloongGu <jack@kuilo.ltd>
2020-09-10 18:43:03 +0800

    upgrade to Ubuntu 20.04

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Some compiling scripts for common open source programs of Linux. Most of them ru
 
 Design for installing and running without root privilieges.
 
-Tested on Ubuntu 18.04.
+Tested on Ubuntu 20.04.
--- vlc-docker.sh
+++ vlc-docker.sh
@@ -2,8 +2,9 @@
 # Ubuntu 20.04 client need to install qt5-gtk-platformtheme
 # Updated on 2020-05-11 for vlc-3.0.10
 # Updated on 2020-07-16 for vlc-3.0.11
+# Updated on 2020-08-07 for Ubuntu 20.04
 set -e
-apt install -y bash-completion build-essential wget cmake unzip libgstreamer-plugins-base1.0-dev nasm ninja-build libpcsclite-dev libglib2.0-dev cmake-curses-gui pigz python3-setuptools bash-completion
+apt install -y bash-completion build-essential wget cmake unzip libgstreamer-plugins-base1.0-dev nasm ninja-build libpcsclite-dev libglib2.0-dev cmake-curses-gui pigz python3-setuptools bash-completion libtasn1-6-dev aom-tools
 apt-get build-dep -y vlc
 
 START_TIME=$(date +%s)
@@ -36,22 +37,23 @@ cmake -DBUILD_SHARED_LIBS=YES ..
 make -j2
 make install
 
-cd $BUILD_PATH
+# NO need since Ubuntu 20.04
+# cd $BUILD_PATH
 # https://launchpad.net/ubuntu/focal/+source/aom
-wget 172.17.0.1:8000/docker/aom_1.0.0.errata1.orig.tar.xz
-tar xf aom_1.0.0.errata1.orig.tar.xz
-cd aom-1.0.0.errata1/
-mkdir my
-cd my/
-cmake ..
-make -j4
-make install
+# wget 172.17.0.1:8000/docker/aom_1.0.0.errata1.orig.tar.xz
+# tar xf aom_1.0.0.errata1.orig.tar.xz
+# cd aom-1.0.0.errata1/
+# mkdir my
+# cd my/
+# cmake ..
+# make -j4
+# make install
 
 cd $BUILD_PATH
 # https://code.videolan.org/videolan/dav1d
-wget 172.17.0.1:8000/docker/dav1d-0.5.2.tar.bz2
-tar xf dav1d-0.5.2.tar.bz2
-cd dav1d-0.5.2
+wget 172.17.0.1:8000/docker/dav1d-0.7.1.tar.bz2
+tar xf dav1d-0.7.1.tar.bz2
+cd dav1d-0.7.1
 meson builddir && cd builddir
 ninja
 ninja install
@@ -134,6 +136,92 @@ mkdir ubuntu && cd ubuntu
 cp /usr/lib/x86_64-linux-gnu/libbluray.so.2 .
 cp /usr/lib/x86_64-linux-gnu/liba52-0.7.4.so .
 cp /usr/lib/x86_64-linux-gnu/libaribb24.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libavcodec.so.58 .
+cp /usr/lib/x86_64-linux-gnu/libavutil.so.56 .
+cp /usr/lib/x86_64-linux-gnu/libBasicUsageEnvironment.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libcddb.so.2 .
+cp /usr/lib/x86_64-linux-gnu/libdca.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libdouble-conversion.so.3 .
+cp /usr/lib/x86_64-linux-gnu/libdvbpsi.so.10 .
+cp /usr/lib/x86_64-linux-gnu/libdvdnav.so.4 .
+cp /usr/lib/x86_64-linux-gnu/libfaad_drm.so.2 .
+cp /usr/lib/x86_64-linux-gnu/libfaad.so.2 .
+cp /usr/lib/x86_64-linux-gnu/libgroupsock.so.8 .
+cp /usr/lib/x86_64-linux-gnu/libgsm.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libixml.so.10 .
+cp /usr/lib/x86_64-linux-gnu/libkate.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libliveMedia.so.77 .
+cp /usr/lib/x86_64-linux-gnu/liblua5.2.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libmad.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libmatroska.so.6 .
+cp /usr/lib/x86_64-linux-gnu/libmpcdec.so.6 .
+cp /usr/lib/x86_64-linux-gnu/libmpeg2.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libnfs.so.13 .
+cp /usr/lib/x86_64-linux-gnu/libopenjp2.so.7 .
+cp /usr/lib/x86_64-linux-gnu/libplacebo.so.7 .
+cp /usr/lib/x86_64-linux-gnu/libprotobuf-lite.so.17 .
+cp /usr/lib/x86_64-linux-gnu/libQt5X11Extras.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libQt5Xml.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libSDL-1.2.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libSDL_image-1.2.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libshine.so.3 .
+cp /usr/lib/x86_64-linux-gnu/libsnappy.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libsoxr.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libswresample.so.3 .
+cp /usr/lib/x86_64-linux-gnu/libswscale.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libupnp.so.13 .
+cp /usr/lib/x86_64-linux-gnu/libUsageEnvironment.so.3 .
+cp /usr/lib/x86_64-linux-gnu/libvdpau.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libvulkan.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libx264.so.155 .
+cp /usr/lib/x86_64-linux-gnu/libx265.so.179 .
+cp /usr/lib/x86_64-linux-gnu/libxvidcore.so.4 .
+cp /usr/lib/x86_64-linux-gnu/libzvbi.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libdvdread.so.7 .
+cp /usr/lib/x86_64-linux-gnu/libQt5Gui.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libQt5Core.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libQt5Widgets.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libQt5Svg.so.5 .
+cp /usr/lib/x86_64-linux-gnu/libpcre2-16.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libavformat.so.58 .
+cp /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libdc1394.so.22 .
+cp /usr/lib/x86_64-linux-gnu/libssh2.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libvncclient.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libxcb-composite.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libspatialaudio.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libsndio.so.7.0 .
+cp /usr/lib/x86_64-linux-gnu/libaom.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libcodec2.so.0.9 .
+cp /usr/lib/x86_64-linux-gnu/libass.so.9 .
+cp /usr/lib/libsidplay2.so.1 .
+cp /usr/lib/libresid-builder.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libchromaprint.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libpostproc.so.55 .
+cp /usr/lib/x86_64-linux-gnu/libgme.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libopenmpt.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libssh-gcrypt.so.4 .
+cp /usr/lib/x86_64-linux-gnu/libmysofa.so.1 .
+
+cd $BUILD_PATH
+mkdir own && cd own
+cp -d /usr/local/lib/lib* .
+cp -d /usr/local/lib/x86_64-linux-gnu/lib* .
+
+echo "Build sucessfully"
+END_TIME=$(date +%s)
+echo "time to build: $((END_TIME-START_TIME))s"
+exit 0
+
+## END
+
+
+# for Ubuntu 18.04
+cd $BUILD_PATH
+mkdir ubuntu && cd ubuntu
+cp /usr/lib/x86_64-linux-gnu/libbluray.so.2 .
+cp /usr/lib/x86_64-linux-gnu/liba52-0.7.4.so .
+cp /usr/lib/x86_64-linux-gnu/libaribb24.so.0 .
 cp /usr/lib/x86_64-linux-gnu/libavcodec.so.57 .
 cp /usr/lib/x86_64-linux-gnu/libavutil.so.55 .
 cp /usr/lib/x86_64-linux-gnu/libBasicUsageEnvironment.so.1 .
@@ -203,75 +291,8 @@ cp /usr/lib/x86_64-linux-gnu/libthreadutil.so.6 .
 cp /usr/lib/libsidplay2.so.1 .
 cp /usr/lib/libresid-builder.so.0 .
 
-cd $BUILD_PATH
-mkdir own && cd own
-cp -d /usr/local/lib/lib* .
-cp -d /usr/local/lib/x86_64-linux-gnu/lib* .
-
-echo "Build sucessfully"
-END_TIME=$(date +%s)
-echo "time to build: $((END_TIME-START_TIME))s"
-exit 0
-
-## END
-
-
-# for Ubuntu 20.04
-mkdir upload
-cd upload/
-cp /usr/lib/x86_64-linux-gnu/libbluray.so.2 .
-cp /usr/lib/x86_64-linux-gnu/liba52-0.7.4.so .
-cp /usr/lib/x86_64-linux-gnu/libaribb24.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libavcodec.so.58 .
-cp /usr/lib/x86_64-linux-gnu/libavutil.so.56 .
-cp /usr/lib/x86_64-linux-gnu/libBasicUsageEnvironment.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libcddb.so.2 .
-cp /usr/lib/x86_64-linux-gnu/libdca.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libdouble-conversion.so.3 .
-cp /usr/lib/x86_64-linux-gnu/libdvbpsi.so.10 .
-cp /usr/lib/x86_64-linux-gnu/libdvdnav.so.4 .
-cp /usr/lib/x86_64-linux-gnu/libfaad_drm.so.2 .
-cp /usr/lib/x86_64-linux-gnu/libfaad.so.2 .
-cp /usr/lib/x86_64-linux-gnu/libgroupsock.so.8 .
-cp /usr/lib/x86_64-linux-gnu/libgsm.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libixml.so.10 .
-cp /usr/lib/x86_64-linux-gnu/libkate.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libliveMedia.so.77 .
-cp /usr/lib/x86_64-linux-gnu/liblua5.2.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libmad.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libmatroska.so.6 .
-cp /usr/lib/x86_64-linux-gnu/libmpcdec.so.6 .
-cp /usr/lib/x86_64-linux-gnu/libmpeg2.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libnfs.so.13 .
-cp /usr/lib/x86_64-linux-gnu/libopenjp2.so.7 .
-cp /usr/lib/x86_64-linux-gnu/libplacebo.so.7 .
-cp /usr/lib/x86_64-linux-gnu/libprotobuf-lite.so.17 .
-cp /usr/lib/x86_64-linux-gnu/libQt5X11Extras.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libQt5Xml.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libSDL-1.2.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libSDL_image-1.2.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libshine.so.3 .
-cp /usr/lib/x86_64-linux-gnu/libsnappy.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libsoxr.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libswresample.so.3 .
-cp /usr/lib/x86_64-linux-gnu/libswscale.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libupnp.so.13 .
-cp /usr/lib/x86_64-linux-gnu/libUsageEnvironment.so.3 .
-cp /usr/lib/x86_64-linux-gnu/libvdpau.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libvulkan.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libx264.so.155 .
-cp /usr/lib/x86_64-linux-gnu/libx265.so.179 .
-cp /usr/lib/x86_64-linux-gnu/libxvidcore.so.4 .
-cp /usr/lib/x86_64-linux-gnu/libzvbi.so.0 .
-cp /usr/lib/x86_64-linux-gnu/libdvdread.so.7 .
-cp /usr/lib/x86_64-linux-gnu/libQt5Gui.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libQt5Core.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libQt5Widgets.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libQt5Svg.so.5 .
-cp /usr/lib/x86_64-linux-gnu/libpcre2-16.so.0 .
-
 # libva.txt
 apt-get build-dep libva
-wget 172.17.0.1:8000/libva_2.1.0.orig.tar.bz2
+wget 172.17.0.1:8000/libva_2.7.0.orig.tar.gz
 ./configure --disable-static --prefix=/home/yiloong/opt/libva2
 
