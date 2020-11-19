02e15532f20f180f1d042114a76d7c95b361f832
YiloongGu <jack@kuilo.ltd>
2020-11-19 19:53:52 +0800

    remove self-compiled libs

--- obs.sh
+++ obs.sh
@@ -2,34 +2,19 @@
 set -e
 
 BUILD_PATH=$PWD
-DOWN_PATH="${HOME}/Downloads"
-OBS_PATH="${HOME}/opt/obs-23.1.0"
+OBS_PATH="/home/yiloong/opt/obs"
+OBS_VER="26.0.2"
 RPATH_FLAGS="-Wl,-rpath,${OBS_PATH}/lib"
-MY_LDFLAGS="-L${OBS_PATH}/lib ${RPATH_FLAGS}"
-MY_CPPFLAGS="-I${OBS_PATH}/include"
-export PKG_CONFIG_PATH=${OBS_PATH}/lib/pkgconfig
+
+apt build-dep obs-studio
+apt install wget checkinstall
 
 cd $BUILD_PATH
-tar xf $DOWN_PATH/x264-stable.tar.bz2
-cd x264-stable
-./configure --enable-static --enable-shared --prefix=${OBS_PATH} #--disable-asm
-make -j4 && make install
-
-#cd $BUILD_PATH
-#tar xf $DOWN_PATH/x265_3.0.tar.gz
-#cd x265_3.0/build/linux
-#cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${OBS_PATH} -DCMAKE_INSTALL_RPATH=${OBS_PATH}/lib ../../source
-#make -j10 && make install
-
-cd $BUILD_PATH
-tar xf $DOWN_PATH/ffmpeg-4.1.3.tar.bz2
-cd ffmpeg-4.1.3
-./configure --prefix=${OBS_PATH} --pkgconfigdir=${OBS_PATH}/lib/pkgconfig --enable-shared --enable-libx264 --enable-libx265 --enable-gpl --extra-ldflags="${MY_LDFLAGS}" --extra-cflags="${MY_CPPFLAGS}"
-make -j10 && make install
-
-cd $BUILD_PATH
-tar xf $DOWN_PATH/obs-studio-23.1.0.tar.gz
-mkdir obs-studio-23.1.0/build
-cd obs-studio-23.1.0/build
+wget 172.17.0.1/obs-studio-${OBS_VER}.tar.gz
+tar xf $DOWN_PATH/obs-studio-${OBS_VER}.tar.gz
+mkdir obs-studio-${OBS_VER}/build
+cd obs-studio-${OBS_VER}/build
 cmake -DUNIX_STRUCTURE=1 -DCMAKE_INSTALL_PREFIX=${OBS_PATH} -DCMAKE_INSTALL_RPATH=${OBS_PATH}/lib ..
+make -j8
+checkinstall --default --pkgname=obs-studio --fstrans=no --backup=no --pkgversion="${OBS_VER}" --deldoc=yes
 
